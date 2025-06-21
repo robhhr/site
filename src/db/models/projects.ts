b1781fd0711aa053ed3317d6dd53731659286c84
robhhr <robertohhr@pm.me>
2025-06-21 16:41:42 -0700

    extend project fetch func

--- src/db/models/projects.ts
+++ src/db/models/projects.ts
@@ -1,8 +1,15 @@
 import {query} from '../db'
 import {tryCatch} from '../../utils/try-catch'
 
-export async function getProjects() {
-  const result = await tryCatch(query('SELECT * FROM projects'))
+export async function getProjects({limit}: {limit?: number} = {}) {
+  const sql = `SELECT * FROM projects ORDER BY created_at DESC LIMIT $1`
+  const result = await tryCatch(query(sql, [limit]))
+
+  if (result.error) {
+    console.error(result.error)
+    return null
+  }
+
   return result.data.rows
 }
 
@@ -10,5 +17,11 @@ export async function getProjectBySlug(slug: string) {
   const result = await tryCatch(
     query('SELECT * FROM projects WHERE slug = $1', [slug]),
   )
+
+  if (result.error) {
+    console.error(result.error)
+    return null
+  }
+
   return result.data.rows[0]
 }
