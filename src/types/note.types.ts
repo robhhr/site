9747a0860e3172a7811a325cd1c19ccf1d6c02e3
robhhr <robertohhr@pm.me>
2025-09-03 01:28:58 -0700

    note type

--- src/types/note.types.ts
+++ src/types/note.types.ts
@@ -1,5 +1,10 @@
-export type Note = {
+export type NoteType = {
   id: number
   title: string
   content: string
+  favorites: number
+  is_pinned: boolean
+  created_at: string
+  updated_at: string
+  tags?: string[]
 }
