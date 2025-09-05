94d33677f1e86733948dfbdd9f5f57429c57d18a
robhhr <robertohhr@pm.me>
2025-09-05 02:36:57 -0700

    project types

--- src/types/project.types.ts
+++ src/types/project.types.ts
@@ -1,5 +1,17 @@
-export type Project = {
+export type MetadataType = {
+  label: string
+  value: string
+  showAdditional: boolean
+  additionalValues: string[]
+}
+
+export type ProjectType = {
   id: number
   title: string
   slug: string
+  content: string
+  metadata: MetadataType[]
+  created_at: string
+  updated_at: string
+  tags?: string[]
 }
